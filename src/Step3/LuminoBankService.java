dc77f7a28c009ebf2f0d24493d3674a1e31d1784
yogeshdarji <yogeshdarji913@gmail.com>
2024-01-08 23:16:16 -0800

    Add step3

---
+++ src/Step3/LuminoBankService.java
@@ -0,0 +1,64 @@
+package Step3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LuminoBankService {
+    static class Transaction {
+        String time;
+        String account;
+        int credit;
+        int debit;
+        String status;
+        String type;
+        int balance;
+
+        public Transaction(String time, String account, int credit, int debit, String status, String type, int balance) {
+            this.time = time;
+            this.account = account;
+            this.credit = credit;
+            this.debit = debit;
+            this.status = status;
+            this.type = type;
+            this.balance = balance;
+        }
+    }
+
+    Map<String, Integer> accountBalances = new HashMap<>();
+    Map<String, List<Transaction>> accountTransactions = new HashMap<>();
+
+    public int getCurrentBalance(String accountId) {
+        return accountBalances.getOrDefault(accountId, 0);
+    }
+
+    public void depositCash(String time, String account, int amount) {
+        int currentBalance = getCurrentBalance(account);
+        currentBalance += amount;
+        accountBalances.put(account, currentBalance);
+
+        Transaction transaction = new Transaction(time, account, amount, 0, "SUCCESS", "ATM", currentBalance);
+        accountTransactions.computeIfAbsent(account, k -> new ArrayList<>()).add(transaction);
+    }
+
+    public boolean withdrawCash(String time, String account, int amount) {
+        int currentBalance = getCurrentBalance(account);
+        String status;
+        if(currentBalance >= amount) {
+            currentBalance -= amount;
+            accountBalances.put(account, currentBalance);
+            status = "SUCCESS";
+        } else {
+            status = "FAILED";
+        }
+
+        Transaction transaction = new Transaction(time, account, 0, amount, status, "ATM", currentBalance);
+        accountTransactions.computeIfAbsent(account, k -> new ArrayList<>()).add(transaction);
+        return status.equals("SUCCESS");
+    }
+
+    public Map<String, List<Transaction>> getAccountTransactions() {
+        return accountTransactions;
+    }
+}
